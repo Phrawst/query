e4e1e885c07d2c5ddf9d3b4d080e3dadb3c5e50c
ODDS <odds@ODDSs-MacBook-Pro.local>
2023-05-03 14:35:48 +0700

    add view display

--- src/app/page/product-list/product-list.component.ts
+++ src/app/page/product-list/product-list.component.ts
@@ -6,8 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
+
+  
+  
+
    showSectionGrid = 0; //default show ข้อมูลแบบ grid
 
+   displayView(sw:number){
+    switch(sw){
+       case 0: 
+        this.showSectionGrid = 0; //show grid view
+        break;
+       default:
+        this.showSectionGrid = 1; //show list view
+    }
+   }
 
 
 }
